81d90801928f20533619243e1e273479060e0b46
Michael Bell <bellm2@tcnj.edu>
2025-02-12 13:29:50 -0500

    Date checker also checks time and duration

--- CheckDateForConflict.rb
+++ CheckDateForConflict.rb
@@ -1,16 +1,29 @@
 # Check if the desired room is available during the desired time
-def checkDateForConflict(reservations, rooms, date, roomsIndex)
+def checkDateForConflict(reservations, rooms, date, roomsIndex, time, duration)
     month = date[5,2].to_i - 1
     day = date[8,2].to_i - 1
 
-    p reservations[4][4]
+    startHour = time[0,2].to_i
+    if time[6,2] == "PM"
+        startHour += 12
+    end
+    durationHour = duration[0,2].to_i
 
-    dailySchedule = reservations[month][day]
-    
+    dailySchedule = reservations.hash[month][day]
     while dailySchedule
-        p rooms[dailySchedule.row]
         if rooms[dailySchedule.row][0] == rooms[roomsIndex][0] && rooms[dailySchedule.row][1] == rooms[roomsIndex][1]
-            return false
+            reservationStart = reservations.table[dailySchedule.index][3]
+            reservationDuration = reservations.table[dailySchedule.index][4]
+
+            reservationStartHour = reservationStart[0,2].to_i
+            if reservationStart[6,2] == "PM"
+                reservationStartHour += 12
+            end
+            reservationEndHour = reservationDuration[0,2].to_i + reservationStartHour
+
+            if !((startHour > reservationEndHour) || (startHour + durationHour < reservationStartHour))
+                return false
+            end
         end
 
         if !dailySchedule
