9c841ec73ea54b36a2717a6ee29225f30066978e
Michael Bell <bellm2@tcnj.edu>
2025-02-11 20:10:41 -0500

    Creates nested linked list of rooms by building and room number

--- SortRoomList.rb
+++ SortRoomList.rb
@@ -3,16 +3,42 @@ require 'csv'
 # Get room data from file and store in a table
 def createRoomList()
     roomList = CSV.parse(File.read("rooms_list.csv"), headers: true)
-    firstPassthrough(roomList)
+    buildings = firstPassthrough(roomList)
     quicksort(roomList, 0, roomList.length()-1)
-    return roomList
+    rooms = Struct.new(:byCapacity, :byBuilding)
+    organizedRooms = rooms.new(roomList, buildings)
+    return organizedRooms
 end
 
-# Convert roomList capacity from strings to ints
+# Creates a nested linked list of buildings and rooms
+# and converts capacity from a string to an int
 def firstPassthrough(rooms)
-    for i in 0..rooms.length()-1 do
+    buildingNode = Struct.new(:building, :contents, :next)
+    roomNode = Struct.new(:index, :roomNum, :next)
+
+    buildings = buildingNode.new(rooms[0][0], roomNode.new(0, rooms[0][1], nil), nil)
+    rooms[0][2] = rooms[0][2].to_i
+
+    for i in 1..rooms.length()-1 do
+        currentBuilding = buildings
+        keepSearching = 1
+        while keepSearching == 1
+            if currentBuilding.building == rooms[i][0]
+                currentBuilding.contents = roomNode.new(i, rooms[i][1], currentBuilding.contents)
+                keepSearching = 0
+            else
+                currentBuilding = currentBuilding.next
+                if !currentBuilding
+                    buildings = buildingNode.new(rooms[i][0], roomNode.new(0, rooms[i][1], nil), buildings)
+                    keepSearching = 0
+                end
+            end
+        end
+
         rooms[i][2] = rooms[i][2].to_i
     end
+
+    return buildings
 end
 
 # Sort roomList by capacity using quicksort
