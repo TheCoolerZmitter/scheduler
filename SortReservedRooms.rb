09c70c6c1e65eca50d193239c54d9ed4382c5be8
Michael Bell <bellm2@tcnj.edu>
2025-02-11 20:24:55 -0500

    "Index" attribute in reservedRooms now reflects the index of the same room in RoomList

--- SortReservedRooms.rb
+++ SortReservedRooms.rb
@@ -2,13 +2,13 @@ require 'csv'
 
 
 # Get reservation data from file and store in a hashtable
-def createReservedRooms()
+def createReservedRooms(roomList)
     reservedRooms = CSV.parse(File.read("reserved_rooms.csv"), headers: true)
-    return organizeReservations(reservedRooms)
+    return organizeReservations(reservedRooms, roomList)
 end
 
 # Organize reservations in hash table by date
-def organizeReservations(reservedRooms)
+def organizeReservations(reservedRooms, roomList)
     reservationNode = Struct.new(:row, :next)
 
     reservationsByDate = Array.new(12){Array.new(31)}
@@ -19,10 +19,30 @@ def organizeReservations(reservedRooms)
         month = date[5,2].to_i - 1
         day = date[8,2].to_i - 1
 
+        building = reservedRooms[i][0]
+        roomNum = reservedRooms[i][1]
+        index = -1
+
+        currentBuilding = roomList
+        while index == -1
+            if building == currentBuilding.building
+                currentRoom = currentBuilding.contents
+                while index == -1
+                    if roomNum == currentRoom.roomNum
+                        index = currentRoom.index
+                    else
+                        currentRoom = currentRoom.next
+                    end
+                end
+            else
+                currentBuilding = currentBuilding.next
+            end
+        end
+
         if reservationsByDate[month][day]
-            reservationsByDate[month][day] = reservationNode.new(i, reservationsByDate[month][day])
+            reservationsByDate[month][day] = reservationNode.new(index, reservationsByDate[month][day])
         else
-            reservationsByDate[month][day] = reservationNode.new(i, nil)
+            reservationsByDate[month][day] = reservationNode.new(index, nil)
         end
     end
 
