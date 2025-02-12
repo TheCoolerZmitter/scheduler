8e2734828c6d84b0c5ccaad929b2f75059ecce4a
Michael Bell <bellm2@tcnj.edu>
2025-02-12 14:11:30 -0500

    Initial creation for checking other constraints

---
+++ CheckOtherConstraints.rb
@@ -0,0 +1,9 @@
+require_relative 'MealCheck'
+require_relative 'ComputerCheck'
+require_relative 'IndividualRoomsCheck'
+
+# Tests for the HackTCNJ specific constraints
+def checkOtherConstraints(reservedRooms, roomList, buildings, newEvent)
+
+    return true
+end
---
+++ MealCheck.rb
@@ -0,0 +1,6 @@
+require_relative 'CheckDateForConflict'
+
+# Checks for rooms in the same building that can be used for meals
+def mealCheck(building, event)
+
+end
