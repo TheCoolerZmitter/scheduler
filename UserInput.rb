486fabc2a6678a63a9396dde610a0d8d2376575f
Michael Bell <bellm2@tcnj.edu>
2025-02-14 13:31:58 -0500

    Fixes issues with leading zeros

--- UserInput.rb
+++ UserInput.rb
@@ -8,6 +8,16 @@ def getUserConstraints()
     while !validInput
         puts "Enter date of event (yyyy-mm-dd): "
         newEvent.date = gets
+
+        # Fixes leading 0 issues
+        if newEvent.date[6,1] == "-"
+            newEvent.date = newEvent.date[0,5] + "0" + newEvent.date[5,4]
+        end
+        if newEvent.date[9,1] == "\n"
+            newEvent.date = newEvent.date[0,8] + "0" + newEvent.date[8,1]
+        end
+
+        # Checks validity
         if newEvent.date.to_i > 2020 && newEvent.date.to_i < 2030 && newEvent.date[5,5].to_i > 0 && newEvent.date[5,5].to_i < 13 && newEvent.date[8,2].to_i > 0 && newEvent.date[8,2].to_i < 32
             validInput = true
         else
@@ -19,7 +29,14 @@ def getUserConstraints()
     while !validInput
         puts "Enter start time of event (hh:mm AM/PM): "
         time = gets
-        if time.to_i > 0 && time.to_i < 13 && time[3,2].to_i >= 0 && time[3,2].to_i < 60 && (time[6,1].casecmp("M") || time[7,1].casecmp("M"))
+
+        # Fixes leading 0 issues
+        if time[1,1] == ":"
+            time = "0" + time
+        end
+
+        # Checks validity
+        if time.to_i > 0 && time.to_i < 13 && time[3,2].to_i >= 0 && time[3,2].to_i < 60 && (time.length == 8 || time.length == 7)
             validInput = true
             # Change 12 to 00 for later calculations
             if time[0,2] == "12"
@@ -34,11 +51,18 @@ def getUserConstraints()
     validInput = false
     while !validInput
         puts "Enter duration of event (hh:mm): "
-    newEvent.duration = gets
-        if newEvent.duration.to_i >= 0 && newEvent.duration.to_i < 100 && newEvent.duration[3,2].to_i >= 0 && newEvent.duration[3,2].to_i < 60
+        newEvent.duration = gets
+
+        # Fixes leading 0 issues
+        if newEvent.duration[1,1] == ":"
+            newEvent.duration = "0" + newEvent.duration
+        end
+
+        # Checks validity
+        if newEvent.duration.to_i >= 4 && newEvent.duration.to_i < 100 && newEvent.duration[3,2].to_i >= 0 && newEvent.duration[3,2].to_i < 60
             validInput = true
         else
-            puts "Invalid duration. Must last between 4 and 100 hours."
+            puts "Invalid duration. Must have duration between 04:00 and 99:59."
         end
     end
 
@@ -46,6 +70,8 @@ def getUserConstraints()
     while !validInput
         puts "Enter number of attendees: "
         newEvent.attendees = gets
+
+        # Checks validity
         if newEvent.attendees.to_i > 0 && newEvent.attendees.to_i <= 1000
             validInput = true
         else
